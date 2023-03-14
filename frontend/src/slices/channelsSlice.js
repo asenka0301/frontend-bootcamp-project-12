17cd5b5466d9070f745c2aa2fe13bcde7e383c71
Nastya <asenka0301@gmail.com>
2023-03-14 15:37:21 +0300

    initialize storage and reducers

---
+++ frontend/src/slices/channelsSlice.js
@@ -0,0 +1,17 @@
+import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+
+const channelsAdapter = createEntityAdapter();
+
+const initialState = channelsAdapter.getInitialState();
+
+const channelsSlice = createSlice({
+  name: 'channel',
+  initialState,
+  reducers: {
+    addChannels: channelsAdapter.addMany,
+  },
+});
+
+export const { actions } = channelsSlice;
+export default channelsSlice.reducer;
+export const selectors = channelsAdapter.getSelectors((state) => state.channels);
---
+++ frontend/src/slices/messagesSlice.js
@@ -0,0 +1,17 @@
+import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+
+const messagesAdapter = createEntityAdapter();
+
+const initialState = messagesAdapter.getInitialState();
+
+const messagesSlice = createSlice({
+  name: 'channel',
+  initialState,
+  reducers: {
+    addMessages: messagesAdapter.addMany,
+  },
+});
+
+export const { actions } = messagesSlice;
+export default messagesSlice.reducer;
+export const selectors = messagesAdapter.getSelectors((state) => state.messages);
