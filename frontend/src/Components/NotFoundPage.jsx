eb2f0eafb6c6197f7a04aced9c91f5e815ded3e3
Nastya <asenka0301@gmail.com>
2023-03-24 17:03:13 +0300

    add NotFoundPage content

--- frontend/src/Components/NotFoundPage.jsx
+++ frontend/src/Components/NotFoundPage.jsx
@@ -1,6 +1,13 @@
-import { Link } from 'react-router-dom';
 import React from 'react';
 
-const NotFound = () => <Link to="/">Go Home</Link>;
+const NotFound = () => (
+  <div className="container h-100 w-50 d-flex flex-column justify-content-center align-items-center bg-white">
+    <h1 className="h1">Ошибка 404</h1>
+    <p className="text-center">
+      Кажется что-то пошло не так! Страница, которую вы запрашиваете, не существует.
+      Возможно она устарела, была удалена или неверный адрес в адресной строке.
+    </p>
+  </div>
+);
 
 export default NotFound;
