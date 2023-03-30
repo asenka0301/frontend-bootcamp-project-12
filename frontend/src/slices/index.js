f6bc1732178090ec4335bce055b0ada79514b8e6
Nastya <asenka0301@gmail.com>
2023-03-30 17:48:31 +0300

    create currentChannelIdReducer

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import messagesReducer from './messagesSlice.js';
 import channelsReducer from './channelsSlice.js';
+import currentChannelIdReducer from './currentChannelSlice';
 
 export default configureStore({
   reducer: {
     messages: messagesReducer,
     channels: channelsReducer,
+    currentChannelId: currentChannelIdReducer,
   },
 });
