0621a5762ca3708baeba7452dcaf17e9fbcd7e43
Nastya <asenka0301@gmail.com>
2023-04-02 13:11:50 +0300

    use store instead of props, fix chat window scrolling

--- frontend/src/components/pages/ChatPage/Components/Messages.jsx
+++ frontend/src/components/pages/ChatPage/Components/Messages.jsx
@@ -1,16 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { selectors as messagesSelectors } from '../../../../slices/messagesSlice';
 
-const Messages = (props) => {
-  const { message, activeChannelId } = props;
-  console.log(message);
+const Messages = () => {
+  const MessageEndRef = useRef(null);
+  const messages = useSelector(messagesSelectors.selectAll);
+  const activeChannelId = useSelector((state) => {
+    const { currentChannelId } = state.currentChannelId;
+    return currentChannelId;
+  });
+
   useEffect(() => {
-    const container = document.getElementById('messages-box');
-    container.scrolTop = container.scrollHeight;
-  }, []);
+    MessageEndRef.current?.scrollIntoView();
+  }, [messages, activeChannelId]);
 
   return (
     <div id="messages-box" className="chat-messages overflow-auto px-5 ">
-      { message && message
+      { messages && messages
         .filter((item) => item.channelId === activeChannelId)
         .map((el) => (
           <div key={el.id} className="text-break mb-2">
@@ -19,6 +25,7 @@ const Messages = (props) => {
             { el.body }
           </div>
         ))}
+      <div ref={MessageEndRef} />
     </div>
   );
 };
